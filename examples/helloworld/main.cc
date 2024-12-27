19ab61519a3047cae3070586628cf89a23569701
Changbin Park <gh4ck3r@gmail.com>
2024-12-27 15:50:05 +0900

    revise example helloworld in C++

--- examples/helloworld/main.cc
+++ examples/helloworld/main.cc
@@ -1,53 +1,21 @@
+#include <algorithm>
 #include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <sys/queue.h>
+#include <eal.hh>
 
-#include <rte_memory.h>
-#include <rte_launch.h>
-#include <rte_eal.h>
-#include <rte_per_lcore.h>
-#include <rte_lcore.h>
-#include <rte_debug.h>
-
-/* Launch a function on lcore. 8< */
-static int
-lcore_hello(__rte_unused void *arg)
+static int lcore_hello([[maybe_unused]] void *)
 {
-	unsigned lcore_id;
-	lcore_id = rte_lcore_id();
-	std::clog << "hello from core " << lcore_id << std::endl;
+	std::clog << "hello from core " << rte_lcore_id() << std::endl;
 	return 0;
 }
-/* >8 End of launching function on lcore. */
 
-/* Initialization of Environment Abstraction Layer (EAL). 8< */
 int
 main(int argc, char **argv)
 {
-	int ret;
-	unsigned lcore_id;
-
-	ret = rte_eal_init(argc, argv);
-	if (ret < 0)
-		rte_panic("Cannot init EAL\n");
-	/* >8 End of initialization of Environment Abstraction Layer */
-
-	/* Launches the function on each lcore. 8< */
-	RTE_LCORE_FOREACH_WORKER(lcore_id) {
-		/* Simpler equivalent. 8< */
-		rte_eal_remote_launch(lcore_hello, NULL, lcore_id);
-		/* >8 End of simpler equivalent. */
-	}
-
-	/* call it on main lcore too */
+	dpdk::EAL eal(argc, argv);
+	std::ranges::for_each(eal.workers(), [] (auto &&w) {
+			w.launch(lcore_hello, nullptr);
+	});
 	lcore_hello(NULL);
-	/* >8 End of launching the function on each lcore. */
-
-	rte_eal_mp_wait_lcore();
-
-	/* clean up the EAL */
-	rte_eal_cleanup();
 
 	return 0;
 }
